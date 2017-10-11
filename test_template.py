908d04c4c60307f7ac1ffc4ee7e1d050720c53d4
cmhaita <chengming_cm@126.com>
2017-10-11 13:06:28 +0800

    python_template

---
+++ test_template.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+@version: ??
+@author: simon.cheng
+@license: Apache Licence 
+@contact: simon.cheng@mavenir.com
+@site: 
+@software: PyCharm Community Edition
+@file: test_template.py
+@time: 10/11/2017 1:04 PM
+"""
+
+
+def func():
+    pass
+
+
+class Main():
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
